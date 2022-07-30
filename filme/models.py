7ba8de58c248e1d4ec4c341dd6a593972621aca8
w2038 <w4gn3r2038@gmail.com>
2022-07-30 18:58:26 -0300

    adicionando tabela no DB

--- filme/models.py
+++ filme/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+LISTA_CATEGORIA = (
+    ("ANALISE", "Análise"),
+    ("PROGRAMACAO", "Programação"),
+    ("APRESENTACAO", "Apresentação"),
+)
+
+# criar o filme
+class Filme(models.Model):
+    titulo = models.CharField(max_length = 100)
+    thumb = models.ImageField(upload_to = 'thumb_filmes')
+    descricao = models.TextField(max_length = 1000)
+    categoria = models.CharField(max_length= 15, choices=LISTA_CATEGORIA)
+    visualizacao = models.IntegerField(default=0)
+    data_criacao = models.DateTimeField(default= timezone.now)
