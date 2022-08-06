77ba81806929d05bb6aec22c6744ca8babbebfad
w2038 <w4gn3r2038@gmail.com>
2022-08-06 11:30:20 -0300

    criando template

--- filme/tests.py
+++ filme/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+nums = [1,2,3,4,5]
+
+def find_max(nums):
+    max_num = float("-inf") # smaller than all other numbers
+    for num in nums:
+        if num > max_num:
+            max_num = num
+    return max_num
+
+print(find_max(nums))
