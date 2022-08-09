356a956fd8c54e0cfd2dd8ed80d8805838254ca9
w2038 <w4gn3r2038@gmail.com>
2022-08-09 17:41:41 -0300

    criando url para cada filme

--- filme/urls.py
+++ filme/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import Homepage, Homefilmes
+from .views import Detalhesfilmes, Homepage, Homefilmes
 
 
 urlpatterns = [
     path('', Homepage.as_view()),
     path('filmes/', Homefilmes.as_view()),
+    path('filmes/<int:pk>', Detalhesfilmes.as_view())
 ]
--- filme/views.py
+++ filme/views.py
@@ -1,22 +1,18 @@
 from django.shortcuts import render
 from .models import Filme
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView
 
-# Create your views here.
-# def homepage(request):
-#     return render(request, "homepage.html")
 
 class Homepage(TemplateView):
     template_name = "homepage.html"
 
 
-# def homefilmes(request):
-#     context = {}
-#     lista_filmes = Filme.objects.all()
-#     context['lista_filmes'] = lista_filmes
-#     return render(request, "homefilmes.html", context)
-
 class Homefilmes(ListView):
     template_name = "homefilmes.html"
     model = Filme
-    # object_list
+    # object_list -> lista de itens do modelo
+
+class Detalhesfilmes(DetailView):
+    template_name = "detalhesfilme.html"
+    model = Filme
+    # objetc -> um item do nosso modelo 
