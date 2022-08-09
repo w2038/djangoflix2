33b8a77b40b3178ce1f53c2c0fd0546b53de04d2
w2038 <w4gn3r2038@gmail.com>
2022-08-09 18:17:08 -0300

    criando links de cada filme

--- filme/urls.py
+++ filme/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from .views import Detalhesfilmes, Homepage, Homefilmes
 
+app_name = 'filme'
 
 urlpatterns = [
-    path('', Homepage.as_view()),
-    path('filmes/', Homefilmes.as_view()),
-    path('filmes/<int:pk>', Detalhesfilmes.as_view())
+    path('', Homepage.as_view(), name = 'homepage'),
+    path('filmes/', Homefilmes.as_view(), name = 'homefilmes'),
+    path('filmes/<int:pk>', Detalhesfilmes.as_view(), name='detalhesfilme'),
 ]
