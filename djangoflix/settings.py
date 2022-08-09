65cf77c934fdee6cbc6c8335cabbc293f11d343a
w2038 <w4gn3r2038@gmail.com>
2022-08-09 17:45:38 -0300

    ajustando o navbat

--- djangoflix/settings.py
+++ djangoflix/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'djangoflix.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
