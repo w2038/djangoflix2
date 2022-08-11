af8d9b05e358c1c4ab645c0a6697f06ba46f667f
w2038 <w4gn3r2038@gmail.com>
2022-08-11 20:19:28 -0300

    editando filmes relacionados

--- filme/views.py
+++ filme/views.py
@@ -16,3 +16,9 @@ class Detalhesfilmes(DetailView):
     template_name = "detalhesfilme.html"
     model = Filme
     # objetc -> um item do nosso modelo 
+
+    def get_context_data(self, **kwargs):
+        context = super(Detalhesfilmes, self).get_context_data(**kwargs)
+        filmes_relacionados = Filme.objects.filter(categoria = self.get_object().categoria)[0:5]
+        context["filmes_relacionados"] = filmes_relacionados
+        return context
