1b48550d8bee15389a0aa135738163542a13e91e
w2038 <w4gn3r2038@gmail.com>
2022-08-13 15:21:18 -0300

    editando o template de pesquisa

--- filme/urls.py
+++ filme/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import Detalhesfilmes, Homepage, Homefilmes
+from .views import Detalhesfilmes, Homepage, Homefilmes, Pesquisafilme
 
 app_name = 'filme'
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', Homepage.as_view(), name = 'homepage'),
     path('filmes/', Homefilmes.as_view(), name = 'homefilmes'),
     path('filmes/<int:pk>', Detalhesfilmes.as_view(), name='detalhesfilme'),
+    path('pesquisa/', Pesquisafilme.as_view(), name='pesquisafilme'),
 ]
