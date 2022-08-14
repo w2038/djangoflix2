a631b4770ebc2e97cbb511c46395ee1984cf6513
w2038 <w4gn3r2038@gmail.com>
2022-08-14 13:13:29 -0300

    Bloqueio de páginas

--- filme/urls.py
+++ filme/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from .views import Detalhesfilmes, Homepage, Homefilmes, Pesquisafilme
+from django.contrib.auth import views as auth_view
 
 app_name = 'filme'
 
@@ -8,4 +9,6 @@ urlpatterns = [
     path('filmes/', Homefilmes.as_view(), name = 'homefilmes'),
     path('filmes/<int:pk>', Detalhesfilmes.as_view(), name='detalhesfilme'),
     path('pesquisa/', Pesquisafilme.as_view(), name='pesquisafilme'),
+    path('login/', auth_view.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_view.LogoutView.as_view(template_name='logout.html'), name='logout'),
 ]
--- filme/views.py
+++ filme/views.py
@@ -1,18 +1,20 @@
 from django.shortcuts import render
 from .models import Filme
 from django.views.generic import TemplateView, ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 
 class Homepage(TemplateView):
     template_name = "homepage.html"
 
 
-class Homefilmes(ListView):
+class Homefilmes(LoginRequiredMixin, ListView):
     template_name = "homefilmes.html"
     model = Filme
     # object_list -> lista de itens do modelo
 
-class Detalhesfilmes(DetailView):
+class Detalhesfilmes(LoginRequiredMixin, DetailView):
     template_name = "detalhesfilme.html"
     model = Filme
     # objetc -> um item do nosso modelo 
@@ -31,7 +33,7 @@ class Detalhesfilmes(DetailView):
         context["filmes_relacionados"] = filmes_relacionados
         return context
 
-class Pesquisafilme(ListView):
+class Pesquisafilme(LoginRequiredMixin, ListView):
     template_name = "pesquisa.html"
     model = Filme
 
